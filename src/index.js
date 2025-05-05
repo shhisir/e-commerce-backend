ef012b8bd8b7a3de25a3765fe3f50b81c0216c4f
Shisir <shisiraryal555@gmail.com>
2025-05-05 16:32:32 +0545

    set up

---
+++ src/index.js
@@ -0,0 +1,9 @@
+
+  const express = require("express")
+  const dotenv = require("dotenv")
+  dotenv.config()
+    const app = express()
+
+    app.listen(prototype,() =>{
+        console.log(`server is listening to port ${port}`)
+    })
